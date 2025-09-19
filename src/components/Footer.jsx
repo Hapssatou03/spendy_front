f0c27e5cd3d931b2914a49f2e07e4ca98bf3255b
Hapssatou <hapssatou.sy@dadi-tech.net>
2025-09-19 15:33:06 +0200

    Rename Footer files with correct case

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
-import "./footer.css";
+import "./Footer.css";
 
 const Footer = () => {
   const { isAuthenticated } = useAuth();
